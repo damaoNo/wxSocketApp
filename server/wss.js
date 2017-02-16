43b7eaee7bd322df5a74e8c98449ba448ebddab0
陈超 <chenchao@eastmoney.com>
2017-02-16 12:37:10 +0800

    fix

--- server/wss.js
+++ server/wss.js
@@ -5,7 +5,7 @@
 var https = require('https');
 var fs = require('fs');
 var ws = require('ws');
-var port = 80;
+var port = 443;
 
 var options = {
       key: fs.readFileSync('./private.pem')
@@ -16,7 +16,7 @@ var options = {
 var server = https.createServer(options, function (req, res) {
     //res.writeHead(403);
     res.end('This is a  WebSockets server!\n');
-}).listen(80, function () {
+}).listen(port, function () {
     console.log('Https server started at port: ' + port);
 });
 
