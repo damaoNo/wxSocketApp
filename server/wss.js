bf3bb56bcfb5fedffa7081f79cba7f606fd2ccac
陈超 <chenchao@eastmoney.com>
2017-02-16 17:02:31 +0800

    fix

--- server/wss.js
+++ server/wss.js
@@ -8,8 +8,29 @@ var user = require('./user');
 module.exports = {
     init: function(WebSocket, wss, socket) {
         socket.send(new Date().toLocaleString() + '：哈喽,来自地球的小伙伴 :)');
+
         socket.on('message', function(message) {
-            console.log('收到客户端消息：', message);
+            console.log('收到客户端消息：', JSON.stringify(message));
+            var msgType = message.type;
+            var data = message.data;
+            msgHandler(msgType, data);
         });
+
+        var msgHandlers = {
+            getUser: function (data) {
+                socket.send({data: user.getUsers(), status: 1, message: 'OK'});
+            },
+            otherWise: function (data) {
+                socket.send({data: '', status: 0, message: 'Error message type!'});
+            }
+        };
+
+        function msgHandler(type, data) {
+            if(msgHandlers[type]){
+                msgHandlers[type](data);
+            }else{
+                msgHandlers.otherWise(data);
+            }
+        }
     }
 };
