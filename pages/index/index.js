2d19d89700c478efd8c7725648fe24f7d6f9e65a
陈超 <chenchao@eastmoney.com>
2017-02-16 15:09:53 +0800

    fix

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,55 +2,110 @@
  * Created by ChenChao on 2016/9/27.
  */
 
+var app = getApp();
 var wssTimer = null;
+var isAutoConnect = false;
+var message = [];
 
-    Page({
+Page({
     data: {
+        userInfo: {},
+        sysInfo: {},
+        networkType: '',
         isOpen: false,
-        message: ''
+        statusMsg: '已断开',
+        message: message
     },
     onLoad: function () {
+        this.userInfo();
+        this.wssInit();
+    },
+    userInfo: function () {
+        var that = this;
+        //调用应用实例的方法获取全局数据
+        app.getUserInfo(function (userInfo) {
+            //更新数据
+            that.setData({
+                userInfo: userInfo
+            });
+        });
+        wx.getSystemInfo({
+            success: function(res) {
+                that.setData({
+                    sysInfo: res
+                });
+            }
+        });
+        wx.getNetworkType({
+            success: function(res) {
+                that.setData({
+                    networkType: res.networkType
+                });
+            }
+        });
+    },
+    wssInit: function () {
         var $this = this;
-        connect();
+        $this.connectWss();
         wx.onSocketError(function(res) {
             console.log('WebSocket连接打开失败，请检查！', res);
+            $this.setData({
+                isOpen: false,
+                statusMsg: '已断开'
+            });
         });
         wx.onSocketOpen(function(res) {
             console.log('WebSocket连接已打开！', res);
-            console.log('5秒后将自动断开！');
             $this.setData({
-                isOpen: true
+                isOpen: true,
+                statusMsg: '已连接'
             });
-            setTimeout(function () {
-                wx.closeSocket();
-            }, 5000);
+            if(isAutoConnect){
+                console.log('10秒后将自动断开！');
+                setTimeout(function () {
+                    wx.closeSocket();
+                }, 10000);
+            }
         });
         wx.onSocketClose(function(res) {
             console.log('WebSocket 已断开！');
             $this.setData({
-                isOpen: false
+                isOpen: false,
+                statusMsg: '已断开'
             });
             //断开重连
-            console.log('2秒后尝试重新连接...');
-            wssTimer = setInterval(function () {
-                if(wssTimer && $this.data.isOpen){
-                    clearInterval(wssTimer);
-                }else{
-                    connect();
-                }
-            }, 2000);
+            if(isAutoConnect){
+                console.log('2秒后尝试重新连接...');
+                wssTimer = setInterval(function () {
+                    if(wssTimer && $this.data.isOpen){
+                        clearInterval(wssTimer);
+                    }else{
+                        $this.connectWss();
+                    }
+                }, 2000)
+            }
         });
         wx.onSocketMessage(function(res) {
             console.log('收到服务器内容：', res);
+            message.push(res.data);
             $this.setData({
-                message: res.data
+                message: message
             });
         });
-
-        function connect() {
-            wx.connectSocket({
-                url: 'wss://www.playgame365.xyz'
-            });
-        }
+    },
+    connectWss: function () {
+        this.setData({
+            statusMsg: '连接中...'
+        });
+        connect();
+    },
+    disconnectWss: function () {
+        wx.closeSocket();
     }
 });
+
+function connect() {
+    wx.connectSocket({
+        url: 'wss://www.playgame365.xyz'
+    });
+}
--- server/wss.js
+++ server/wss.js
@@ -5,34 +5,39 @@
 var https = require('https');
 var fs = require('fs');
 var ws = require('ws');
+var express = require('express');
 var port = 443;
 
+var app = express();
+
+app.get('/.well-known/pki-validation/fileauth.htm', function (req, res) {
+    res.setHeader('content-type', 'text/plain');
+    res.send(fs.readFileSync('./fileauth.htm'));
+});
+
+app.use(function (req, res) {
+    res.send({ msg: "这是 shaman 的 socket 服务器！" });
+});
+
 var options = {
-      key: fs.readFileSync('./private.pem')
+    key: fs.readFileSync('./private.pem')
     , cert: fs.readFileSync('./file.crt')
     //, passphrase: '1234'
 };
-
-var server = https.createServer(options, function (req, res) {
-    //res.writeHead(403);
-    res.end('This is a  WebSockets server!\n');
-}).listen(port, function () {
-    console.log('Https server started at port: ' + port);
+var server = https.createServer(options, app).listen(port, function listening() {
+    console.log('Listening on %d', server.address().port);
 });
-
-
 var wss = new ws.Server( {
     server: server,
     verifyClientL: function (info) {
         console.log('收到来自 ' + info.origin + ' 的连接...');
     }
 } );
-wss.on( 'connection', function ( wsConnect ) {
-    console.log('some one connected!');
 
-    wsConnect.send(new Date().toLocaleString() + '：Hello 来自地球的小伙伴...');
-
+wss.on('connection', function connection(wsConnect) {
+    console.log(1111111);
+    wsConnect.send(new Date().toLocaleString() + '：哈喽,来自地球的小伙伴 :)');
     wsConnect.on( 'message', function ( message ) {
-        console.log( message );
+        console.log('收到客户端消息：', message);
     });
 });
