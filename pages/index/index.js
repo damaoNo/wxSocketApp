332f3f55c7920c7c9c43653aea90c347f16c77df
陈超 <chenchao@eastmoney.com>
2017-02-16 12:36:18 +0800

    fix

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,6 +8,14 @@ Page({
     data: {
     },
     onLoad: function () {
-        //
+        wx.connectSocket({
+            url: 'wss://www.playgame365.xyz'
+        });
+        wx.onSocketOpen(function(res) {
+            console.log('WebSocket连接已打开！', res);
+        });
+        wx.onSocketMessage(function(res) {
+            console.log('收到服务器内容：', res);
+        })
     }
 });
--- server/wss.js
+++ server/wss.js
@@ -24,6 +24,9 @@ var server = https.createServer(options, function (req, res) {
 var wss = new ws.Server( { server: server } );
 wss.on( 'connection', function ( wsConnect ) {
     console.log('some one connected!');
+
+    wsConnect.send('Hello 来自地球的小伙伴...');
+
     wsConnect.on( 'message', function ( message ) {
         console.log( message );
     });
