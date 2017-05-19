ea7e7ffe8f79f9ac197d867d4de2b27bcef1d828
chenchao <chenchao@eastmoney.com>
2017-05-19 13:30:03 +0800

    fix

--- server/blog.nodejser.site.js
+++ server/blog.nodejser.site.js
@@ -8,7 +8,21 @@ const port = 80;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
+    res.end(`
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <title>Title</title>
+            <style type="text/css">
+                html,body,iframe{margin: 0; padding: 0; height: 100%; width: 100%}
+            </style>
+        </head>
+        <body>
+            <iframe src="http://s30735.rxsg.ledu.com/" frameborder="0"></iframe>
+        </body>
+        </html>
+    `);
 });
 
 server.listen(port, () => {
