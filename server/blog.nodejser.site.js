2dbc1c7241999475648cd26ff569e5343e933413
chenchao <chenchao@eastmoney.com>
2017-05-19 13:34:37 +0800

    fix

--- server/blog.nodejser.site.js
+++ server/blog.nodejser.site.js
@@ -7,7 +7,7 @@ const port = 80;
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'xml/html');
+    res.setHeader('Content-Type', 'text/html;charset=utf-8');
     res.end(`
         <!DOCTYPE html>
         <html lang="en">
