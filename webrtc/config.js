e83ad09a62a6e931788b085eec76c9007d636c22
chenchao <chenchao@eastmoney.com>
2017-07-25 17:09:56 +0800

    fix

---
+++ webrtc/config.js
@@ -0,0 +1,24 @@
+/**
+ * Created by ChenChao on 2017/7/21.
+ */
+
+var fs = require('fs');
+var path = require('path');
+
+module.exports = {
+    //证书
+    key: fs.readFileSync(__dirname + '/keys/214025060130250.key'),
+    cert: fs.readFileSync(__dirname + '/keys/214025060130250.pem'),
+    
+    //是否为双向实时视频
+    bothWay: true,
+    //保存格式可选 [.mp4, .webm]
+    suffix: '.mp4',
+    //临时视频文件目录
+    tempVideoPath: path.resolve(__dirname, '../videos_temp'),
+    //确认的视频文件目录
+    videoPath: path.resolve(__dirname, '../videos'),
+    
+    //mp4 url访问路径
+    videoUrl: 'http://180.163.42.201:8009/'
+};
--- webrtc/server.js
+++ webrtc/server.js
@@ -8,12 +8,12 @@ var fs = require('fs');
 var https = require('https');
 var WebSocket = require('ws');
 var Uuid = require('uuid');
+var config = require('./config');
 var port = 443;
-var options = {
-    key: fs.readFileSync(__dirname + '/keys/214025060130250.key'),
-    cert: fs.readFileSync(__dirname + '/keys/214025060130250.pem')
-};
-var server = https.createServer(options, function (req, res) {
+var server = https.createServer({
+    key: config.key,
+    cert: config.cert
+}, function (req, res) {
     res.writeHead(200);
     res.end('hello\n');
 }).listen(port, function listening() {
@@ -22,22 +22,38 @@ var server = https.createServer(options, function (req, res) {
 var wss = new WebSocket.Server({
     server: server,
     verifyClient: function (info, next) {
-        console.log(url.parse(info.req.url).query);
+        //console.log(url.parse(info.req.url).query);
 	//todo 添加验证规则
         next(true);
     }
 });
-var config = {
-    //保存格式可选 [.mp4, .webm]
-    suffix: '.mp4',
-    //临时视频文件目录
-    tempVideoPath: path.resolve(__dirname, '../videos_temp'),
-    //确认的视频文件目录
-    videoPath: path.resolve(__dirname, '../videos')
+
+//定义消息广播
+wss.broadcast = function broadcast(message, verify) {
+    wss.clients.forEach(function each(client) {
+        if (client.readyState === WebSocket.OPEN) {
+            if(!verify){
+                //公共频道
+                client.send(message);
+            }else{
+                //私发客服
+                if(client.verify === verify){
+                    if(typeof message === 'object'){
+                        //双向实时
+                        client.send(message);
+                    }
+                    if(typeof message === 'string'){
+                        client.send(message);
+                    }
+                }
+            }
+        }
+    });
 };
 
 wss.on('connection', function connection(socket) {
     var fileWriteStream = null;
+    socket.verify = '';
     socket.send(JSON.stringify({
         action: 'apply format',
         suffix: config.suffix
@@ -46,12 +62,20 @@ wss.on('connection', function connection(socket) {
         //二进制数据
         if(typeof data === 'object'){
             fileWriteStream && fileWriteStream.write(data);
+            if(config.bothWay){
+                wss.broadcast(data, 'admin');
+            }
         }
         //其他动作
         if(typeof data === 'string'){
             var actionObj = JSON.parse(data);
             var action = actionObj.action;
+            var token = actionObj.token;
             var actionFn = {
+                'ping': function () {
+                    //console.log('admin ping:', actionObj.verify);
+                    socket.verify = actionObj.verify;
+                },
                 'record start': function () {
                     var fileName = Uuid.v4() + config.suffix;
                     var tempFilePath = path.join(config.tempVideoPath, fileName);
@@ -71,6 +95,12 @@ wss.on('connection', function connection(socket) {
                                 socket.send(JSON.stringify({action: 'saved failed', errorMessage: '视频保存失败'}));
                             }else{
                                 socket.send(JSON.stringify({action: 'saved success', path: ['videos', fileName].join('/')}));
+                                wss.broadcast(JSON.stringify({
+                                    action: 'user recode over',
+                                    user: token,
+                                    time: new Date().toLocaleString(),
+                                    video: [config.videoUrl, fileName].join('')
+                                }), 'admin');
                             }
                         });
                     });
