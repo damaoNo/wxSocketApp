cf10354e0774f436389e5cdd76843ca6e241b938
chenchao <chenchao@eastmoney.com>
2017-10-20 11:35:58 +0800

    fix

---
+++ webrtc/clinet-server/app.js
@@ -0,0 +1,70 @@
+/**
+ * Created by ChenChao on 2017/2/16.
+ */
+
+var https = require('https');
+var fs = require('fs');
+var util = require('util');
+var express = require('express');
+var port = 8888;
+var app = express();
+
+app.get('/', function(req, res) {
+    res.sendfile(__dirname + '/index.html');
+});
+
+app.get('/index_1', function(req, res) {
+    res.sendfile(__dirname + '/index_1.html');
+});
+
+app.get('/service-single-track', function(req, res) {
+    res.sendfile(__dirname + '/admin-service-single-track.html');
+});
+
+app.get('/admin-service-single-track.js', function(req, res) {
+    res.sendfile(__dirname + '/admin-service-single-track.js');
+});
+
+app.get('/service-both-way', function(req, res) {
+    res.sendfile(__dirname + '/admin-service-both-way.html');
+});
+
+app.get('/admin-service-both-way.js', function(req, res) {
+    res.sendfile(__dirname + '/admin-service-both-way.js');
+});
+
+app.get('/vue2.0.min.js', function(req, res) {
+    res.sendfile(__dirname + '/vue2.0.min.js');
+});
+
+app.get('/style.css', function(req, res) {
+    res.sendfile(__dirname + '/style.css');
+});
+
+app.get('/main.js', function(req, res) {
+    res.sendfile(__dirname + '/main.js');
+});
+
+app.get('/main_1.js', function(req, res) {
+    res.sendfile(__dirname + '/main_1.js');
+});
+
+app.get('/common.js', function(req, res) {
+    res.sendfile(__dirname + '/common.js');
+});
+
+app.get('/adapter-latest.js', function(req, res) {
+    res.sendfile(__dirname + '/adapter-latest.js');
+});
+
+app.use(function (req, res) {
+    res.send('hello world!');
+});
+
+var options = {
+    key: fs.readFileSync(__dirname + '/keys/214025060130250.key'),
+    cert: fs.readFileSync(__dirname + '/keys/214025060130250.pem')
+};
+var server = https.createServer(options, app).listen(port, function listening() {
+    console.log('Listening on %d', server.address().port);
+});
